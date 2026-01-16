ae9c870c96292f746bc351b14ed0080db3938d7b
nilampaksi95 <nilampaksi95@gmail.com>
2026-01-16 23:09:12 +0800

    Update: Adding Mirror Monitoring images to ECR

--- terraform/ecr.tf
+++ terraform/ecr.tf
@@ -10,3 +10,29 @@ resource "aws_ecr_repository" "app" {
 
 }
 
+resource "aws_ecr_repository" "node_exporter" {
+  name                 = "monitoring/node-exporter"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "prometheus" {
+  name                 = "monitoring/prometheus"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
+
+resource "aws_ecr_repository" "grafana" {
+  name                 = "monitoring/grafana"
+  image_tag_mutability = "MUTABLE"
+
+  image_scanning_configuration {
+    scan_on_push = true
+  }
+}
